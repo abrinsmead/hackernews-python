14f6baaa0dbb9dffe3e0ea4ac6e320afaa946708
Alex Brinsmead <alexbrinsmead@aviamedia.com>
2014-10-07 15:03:30 -0700

    license and setuptools config

---
+++ setup.py
@@ -0,0 +1,34 @@
+import os
+
+from setuptools import setup
+
+def read(*paths):
+    """Build a file path from *paths* and return the contents."""
+    with open(os.path.join(*paths), 'r') as f:
+        return f.read()
+
+setup(
+    name='hackernews-python',
+    version='0.1.0',
+    description='A Python wrapper for the official Hacker News API (v0)',
+    long_description=(read('README.rst')),
+    url='https://github.com/abrinsmead/hackernews-python',
+    license='MIT',
+    author='Alex Brinsmead',
+    author_email='alex.brinsmead@dataloft.com',
+    py_modules=['hackernews'],
+    install_requires=['requests>=2.4.3'],
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from hackernews import HackerNews
 
-
+from hackernews import HackerNews
 hn = HackerNews()
-print hn.top_stories()
+
+print(hn.top_stories())
